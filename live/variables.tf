9cfc6997c72d2a3437a65ed499ed2f0e653724bd
tconuorah <Terrenceonuorah@gmail.com>
2026-01-01 23:26:31 -0600

    vpc and ecr

--- live/variables.tf
+++ live/variables.tf
@@ -41,28 +41,3 @@ variable "ecr_repositories" {
   description = "List of ECR repository names to create"
 }
 
-# EKS inputs
-variable "kubernetes_version" {
-  type        = string
-  description = "EKS Kubernetes version (e.g., 1.29)"
-}
-
-variable "node_instance_types" {
-  type        = list(string)
-  description = "EC2 instance types for node group"
-}
-
-variable "min_size" {
-  type        = number
-  description = "Minimum size of the node group"
-}
-
-variable "max_size" {
-  type        = number
-  description = "Maximum size of the node group"
-}
-
-variable "desired_size" {
-  type        = number
-  description = "Desired size of the node group"
-}
