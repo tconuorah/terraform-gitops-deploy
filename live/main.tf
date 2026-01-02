688b592da7fdd98447cc0511240cafb6aeecb5b8
tconuorah <Terrenceonuorah@gmail.com>
2026-01-01 23:25:04 -0600

    vpc and ecr

--- live/main.tf
+++ live/main.tf
@@ -17,20 +17,4 @@ module "ecr" {
   tags             = local.tags
 }
 
-module "eks" {
-  source = "../modules/eks"
 
-  name               = local.name
-  region             = var.region
-  kubernetes_version = var.kubernetes_version
-
-  vpc_id             = module.vpc.vpc_id
-  private_subnet_ids = module.vpc.private_subnet_ids
-
-  node_instance_types = var.node_instance_types
-  min_size            = var.min_size
-  max_size            = var.max_size
-  desired_size        = var.desired_size
-
-  tags = local.tags
-}
